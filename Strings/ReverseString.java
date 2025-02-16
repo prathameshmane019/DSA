3c6b5bf047c7f1fdd7b09d09d1b7b3d7e6fa406a
prathameshmane019 <maneprathamesh019@gmail.com>
2025-02-16 19:32:17 +0530

    reverse string char only

---
+++ Strings/ReverseString.java
@@ -0,0 +1,67 @@
+
+// 917. Reverse Only Letters
+// Easy
+// Topics
+// Companies
+// Hint
+// Given a string s, reverse the string according to the following rules:
+
+// All the characters that are not English letters remain in the same position.
+// All the English letters (lowercase or uppercase) should be reversed.
+// Return s after reversing it.
+
+ 
+
+// Example 1:
+
+// Input: s = "ab-cd"
+// Output: "dc-ba"
+// Example 2:
+
+// Input: s = "a-bC-dEf-ghIj"
+// Output: "j-Ih-gfE-dCba"
+// Example 3:
+
+// Input: s = "Test1ng-Leet=code-Q!"
+// Output: "Qedo1ct-eeLg=ntse-T!"
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 100
+// s consists of characters with ASCII values in the range [33, 122].
+// s does not contain '\"' or '\\'.
+
+
+public class ReverseString {
+    public static String reverseOnlyLetters(String s) {
+        int l=0;
+        int r = s.length()-1;
+        char[] res = new char[r+1];
+
+        while (l<=r) {
+            if (Character.isAlphabetic(s.charAt(l)) && Character.isAlphabetic(s.charAt(r))) {
+                res[l]=  s.charAt(r);
+                res[r]= s.charAt(l);
+                l++;
+                r--;
+            }
+            else if(!Character.isAlphabetic(s.charAt(l))){
+                res[l]=s.charAt(l);
+                 l++;
+             }
+             else if(!Character.isAlphabetic(s.charAt(r))){
+                res[r]=s.charAt(r);
+                r--;
+
+          }
+        }
+
+
+        return new String(res);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseOnlyLetters("a-bC-dEf-ghIj"));
+    }
+}
