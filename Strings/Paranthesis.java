48acd303d984c6e8f88a730a8088c5416cc02c49
prathameshmane019 <maneprathamesh019@gmail.com>
2025-02-16 20:03:40 +0530

    Valid paranthesis 20

---
+++ Strings/Paranthesis.java
@@ -0,0 +1,72 @@
+// 20. Valid Parentheses
+// Easy
+// Topics
+// Companies
+// Hint
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+// Example 1:
+
+// Input: s = "()"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "()[]{}"
+
+// Output: true
+
+// Example 3:
+
+// Input: s = "(]"
+
+// Output: false
+
+// Example 4:
+
+// Input: s = "([])"
+
+// Output: true
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of parentheses only '()[]{}'.
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Paranthesis {
+    static boolean isValid(String s) {
+                Stack<Character> stack = new Stack<>();
+        Map<Character, Character> mapping = new HashMap<>();
+        mapping.put(')', '(');
+        mapping.put('}', '{');
+        mapping.put(']', '[');
+
+        for (char c : s.toCharArray()) {
+            if (mapping.containsValue(c)) {
+                stack.push(c);
+            } else if (mapping.containsKey(c)) {
+                if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();        
+    } 
+
+    public static void main(String[] args) {
+        System.out.println(isValid("(})"));
+    }
+
+}
