5526b84c2016ed636966e8021bb7d0cf9f7c41f2
prathameshmane019 <maneprathamesh019@gmail.com>
2025-02-23 22:12:36 +0530

    Find First and last

--- Arrays/FindStartAndEnd.java
+++ Arrays/FindStartAndEnd.java
@@ -1,3 +1,66 @@
-public class FindStartAndEnd {
+// 34. Find First and Last Position of Element in Sorted Array
+// Medium
+// Topics
+// Companies
+// Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+// If target is not found in the array, return [-1, -1].
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+
+// Input: nums = [5,7,7,8,8,10], target = 8
+// Output: [3,4]
+// Example 2:
+
+// Input: nums = [5,7,7,8,8,10], target = 6
+// Output: [-1,-1]
+// Example 3:
+
+// Input: nums = [], target = 0
+// Output: [-1,-1]
+
+// Constraints:
+
+// 0 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+// nums is a non-decreasing array.
+// -109 <= target <= 109
+
+public class FindStartAndEnd { 
+        public int[] searchRange(int[] nums, int target) {
+            int[] result = {-1, -1};
+            int left = binarySearch(nums, target, true);
+            int right = binarySearch(nums, target, false);
+            result[0] = left;
+            result[1] = right;
+            return result;        
+        }
     
-}
+        private int binarySearch(int[] nums, int target, boolean isSearchingLeft) {
+            int left = 0;
+            int right = nums.length - 1;
+            int idx = -1;
+    
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                
+                if (nums[mid] > target) {
+                    right = mid - 1;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    idx = mid;
+                    if (isSearchingLeft) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+            }
+    
+            return idx;
+        }
+    
+    }
