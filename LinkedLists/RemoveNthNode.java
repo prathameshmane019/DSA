d7e08e05d46e4d40600812a0a4f02640d8745abc
prathameshmane019 <maneprathamesh019@gmail.com>
2025-02-23 13:34:07 +0530

    remove nth node from last

--- LinkedLists/RemoveNthNode.java
+++ LinkedLists/RemoveNthNode.java
@@ -31,7 +31,50 @@
 
 public class RemoveNthNode {
 
+     class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+     }
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+
+        for (int i = 0; i < n; i++) {
+            head=head.next;
+        }
+
+        while (head!=null) {
+            head=head.next; 
+            dummy=dummy.next;
+        }
+        if (head==null) {
+            dummy.next =dummy.next.next;
+        }
+
+        return first.next;
+    }
+
+    public static void main(String[] args) {
+        RemoveNthNode rn = new RemoveNthNode();
+        ListNode head = rn.new ListNode(1);
+         head.next = rn.new ListNode(3);
+         head.next.next = rn.new ListNode(4);
+         head.next.next.next = rn.new ListNode(5);
+        head.next.next.next.next = rn.new ListNode(2);
+
         
+        System.out.println(rn.removeNthFromEnd(head, 2));
+
+        // print list
+        ListNode curr =head;
+        while (curr!=null) {
+            System.out.println(curr.val);
+            curr = curr.next;
+        }
     }
 }
